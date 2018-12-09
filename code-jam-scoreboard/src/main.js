00db8b4648f9bd665ffce62bcc98fe9a4b6fca65
Katsiaryna Ramaniuk <katia-l@mail.ru>
2018-12-09 14:45:25 +0300

    feat: add data to table

--- code-jam-scoreboard/src/main.js
+++ code-jam-scoreboard/src/main.js
@@ -5,17 +5,35 @@ function showContent() {
     const table = document.getElementsByTagName('table')[0];
     const tbody = document.getElementsByTagName('tbody')[0];
     const tr = table.getElementsByClassName('names')[0];
+    const td = tr.children[0];
    
-    
+
     const item = users.map(elem => elem.displayName)
+    const rounds = sessions[0].rounds;
 
     for (let i = 0; i < item.length; i++) {
         const rowName = document.importNode(tr, true);
         rowName.children[0].textContent = item[i];
+        const uid = users[i].uid;
+        rowName.children[0].setAttribute('uid', uid);
+
+        const totalTimeNode = document.importNode(td, true);
+        rowName.appendChild(totalTimeNode);
+
+        let totalTime = 0;
+        rounds.forEach(elem => {
+            const time = elem.solutions[uid] && +elem.solutions[uid].time.$numberLong || 151
+            const userTd = document.importNode(td, true);
+            userTd.textContent = time;
+            totalTime = totalTime + time
+            rowName.appendChild(userTd);
+        })
+
+        totalTimeNode.textContent = totalTime;
         tbody.appendChild(rowName);
     }
+    tbody.removeChild(tr);
 
-   
     const trHead = document.getElementById('table-head');
     const puzzles = sessions[0].puzzles;
     const puzzleRow = puzzles.map(item => item.name);
@@ -29,10 +47,26 @@ showContent()
 
 function load() {
     console.log(sessions);
-    //console.log(sessions[0].puzzles);
-    const puzzles = sessions[0].puzzles;
-   
-    console.log(users);
-   
+    const timeData = sessions[0].rounds[0].solutions;
+         console.log(timeData);
+const time = Object.values(timeData);
+const timeInfo = Object.values(time);
+
+   console.log(time);
+    // const puzzles = sessions[0].puzzles;
+   // console.log(sessions[0].rounds[0].solutions)
+   // console.log(Object.values(sessions[0].rounds[0].solutions))
+    //const dataTime = Object.values(sessions[0].rounds[0].solutions);
+    //const time = dataTime[0];
+    //console.log(time.time)
+    //console.log(Object.keys(time))
+    //console.log(dataTime)
+    //const { code, time, correct } = dataTime[0];
+   // console.log(time);
+   // console.log(time.$numberLong);
+    //console.log(code); //add to the tooltype
+   // console.log(correct); //check the result task
+    // console.log(users);
+
 }
 load();
